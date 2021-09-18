109dafa35e447d693284825deec937061a661731
Alexandre Medeiros <medeirosalexandre@gmail.com>
2021-09-18 13:42:09 -0300

    Teste de integração de cozinha

---
+++ src/test/java/com/algaworks/algafood/CadastroCozinhaIntegrationTests.java
@@ -0,0 +1,52 @@
+package com.algaworks.algafood;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import javax.validation.ConstraintViolationException;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.service.CadastroCozinhaService;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class CadastroCozinhaIntegrationTests {
+	
+	@Autowired
+	CadastroCozinhaService cadastroCozinha;
+
+	@Test
+	public void testarCadastroCozinhaComSucesso() {
+		// cenário
+		Cozinha novaCozinha = new Cozinha();
+		novaCozinha.setNome("Chinesa");
+		
+		// ação
+		cadastroCozinha.salvar(novaCozinha);
+		
+		// validação
+		assertThat(novaCozinha).isNotNull();
+		assertThat(novaCozinha.getId()).isNotNull();
+	}
+
+	
+	@Test
+	public void testarCadastroCozinhaSemNome() {
+		assertThrows(ConstraintViolationException.class, () -> {
+
+			Cozinha novaCozinha = new Cozinha();
+			novaCozinha.setNome(null);
+
+			novaCozinha = cadastroCozinha.salvar(novaCozinha);
+
+		});
+	}
+	
+
+}

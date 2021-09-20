8ea9afa07c3e98693e7897dab69992eca302f042
Alexandre Medeiros <medeirosalexandre@gmail.com>
2021-09-19 22:04:18 -0300

    Adicional anotação @Transactional aos métodos que manipulam dados

--- src/main/java/com/algaworks/algafood/domain/service/CadastroFormaPagamentoService.java
+++ src/main/java/com/algaworks/algafood/domain/service/CadastroFormaPagamentoService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.algaworks.algafood.domain.exception.EntidadeEmUsoException;
 import com.algaworks.algafood.domain.exception.FormaPagamentoNaoEncontradaException;
@@ -16,10 +17,12 @@ public class CadastroFormaPagamentoService {
 	@Autowired
 	private FormaPagamentoRepository formaPagamentoRepository;
 	
+	@Transactional
 	public FormaPagamento salvar(FormaPagamento formaPagamento) {
 		return formaPagamentoRepository.save(formaPagamento);
 	}
 	
+	@Transactional
 	public void excluir(Long formaPagamentoId) {
 		try {
 			formaPagamentoRepository.deleteById(formaPagamentoId);
